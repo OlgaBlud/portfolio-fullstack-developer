c3fec1ae2f17141dff95992f2f8183efac488cb4
AllaHanol <156720109+AllaHanol@users.noreply.github.com>
2024-07-04 21:57:03 +0200

    corrections mobile version

--- src/main.js
+++ src/main.js
@@ -1,3 +1,5 @@
+document.getElementById('');
+
 document.getElementById('benefits-action-button').onclick = function () {
   document.getElementById('footer').scrollIntoView({ behavior: 'smooth' });
 };
