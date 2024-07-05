f60ef1a6613734a1e53e5bcca6b9e20834953391
Didkovskyi Davyd <116342774+Daedraglob01@users.noreply.github.com>
2024-07-05 08:54:53 +0300

    Add files via upload

---
+++ src/js/reviews/render-functions.js
@@ -0,0 +1,19 @@
+export const createReviewsList = reviews => {
+    return reviews
+    .map(
+      reviewData => ` 
+     <li class="reviews-item swiper-slide" id="${reviewData._id - 1}">
+           <img
+              class="reviews-avatar"
+              alt="${reviewData.author}"
+              src="${reviewData.avatar_url}" 
+            />          
+          <h3 class="reviews-name">${reviewData.author}</h3>
+          <p class="reviews-text">
+            ${reviewData.review}
+          </p>
+    </li>
+  `
+    )
+    .join('');
+};
