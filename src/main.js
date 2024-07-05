9965d512f518410c3e9226102af7a1730a2a413e
AllaHanol <156720109+AllaHanol@users.noreply.github.com>
2024-07-05 19:00:47 +0200

    benefits-section, main.js mut

--- src/main.js
+++ src/main.js
@@ -1,3 +1,3 @@
-document.getElementById('benefits-action-button').onclick = function () {
-  document.getElementById('footer').scrollIntoView({ behavior: 'smooth' });
-};
+// document.getElementById('benefits-action-button').onclick = function () {
+//   document.getElementById('footer').scrollIntoView({ behavior: 'smooth' });
+// };
