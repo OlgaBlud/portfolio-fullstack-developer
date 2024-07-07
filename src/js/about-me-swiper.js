5c854a26095a8d9721ae1f9471b5a49fce274346
Olga Blud <olga.blud@gmail.com>
2024-07-07 16:25:28 +0300

    swiper work

--- src/js/about-me-swiper.js
+++ src/js/about-me-swiper.js
@@ -1,2 +1,17 @@
-import Swiper from 'swiper';
+// import Swiper from 'swiper';
 import { refs } from './refs';
+import Swiper from 'swiper/bundle';
+
+new Swiper(refs.skillsSwiper, {
+  direction: 'horizontal',
+  loop: true,
+
+  slidesPerView: 2,
+  width: 260,
+  //   spaceBetween: 0,
+  //   slidesPerGroup: 2,
+
+  navigation: {
+    nextEl: '.swiper-button-next',
+  },
+});
