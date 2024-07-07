bf9ebda0f3358b1d86b78eb6b975ff8069afb61c
Tetiana Hrihorash <tmgrigorash@gmail.com>
2024-07-07 14:27:11 +0300

    modal-menu html

--- src/js/refs.js
+++ src/js/refs.js
@@ -1,1 +1,6 @@
-export const refs = {};
+export const refs = {
+  btnClose: document.querySelector('.close-btn'),
+  btnOpenMenu: document.querySelector('btn-open-menu'),
+  modalOverlay: document.querySelector('.modal-overlay'),
+  closeMenu: document.querySelector('.close-menu'),
+};
