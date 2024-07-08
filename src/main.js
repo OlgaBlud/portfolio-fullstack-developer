3c0336cc11dc83bb821b6c167253056cc4789d4d
Bohdan Lytvynets <157913587+Bohdan-Lytvynets@users.noreply.github.com>
2024-07-08 21:56:33 +0200

    Update main.js

--- src/main.js
+++ src/main.js
@@ -3,5 +3,5 @@ import './js/about-me-accordion';
 import './js/about-me-swiper';
 import './js/work-together';
 import './js/reviews/render-functions';
-import './js/reviews/render';
+import './js/reviews/reviews';
 import './js/header';
