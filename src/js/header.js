9f2ec2954397bb2bec254c9fb605a70a29d29968
Tetiana Hrihorash <tmgrigorash@gmail.com>
2024-07-08 03:13:49 +0300

    header-js

--- src/js/header.js
+++ src/js/header.js
@@ -2,15 +2,30 @@ import { refs } from './refs';
 
 //===============Відкриття модального вікна
 refs.btnOpenMenu.addEventListener('click', () => {
-  modalOverlay.classList.remove('.is-open');
+  refs.modalOverlay.classList.add('is-open');
 });
-
 // =================Закриття модального вікна кнопкою
 refs.btnClose.addEventListener('click', () => {
-  modalOverlay.classList.add('.is-open');
+  refs.modalOverlay.classList.remove('is-open');
 });
 
 // =================Закриття модального вікна пр переході за посиланням
-refs.closeMenu.addEventListener('click', () => {
-  modalOverlay.classList.add('.is-open');
+
+refs.close1.addEventListener('click', () => {
+  setTimeout(refs.modalOverlay.classList.remove('is-open'), 2000);
+});
+
+refs.close2.addEventListener('click', () => {
+  refs.modalOverlay.classList.remove('is-open');
+});
+
+const eventStateMenu = refs.menuTablet.classList.toggle('is-open');
+// const eventStateModal = refs.modal.classList.toggle('is-open');
+
+refs.menuTablet.addEventListener('click', e => {
+  e.preventDefault();
+  if (eventStateMenu === true) {
+    refs.modalOverlay.classList.remove('is-open');
+  }
+  refs.modalOverlay.classList.add('is-open');
 });
