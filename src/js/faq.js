b993a7e31d64dc1300cb14cfa3b9c51228442248
Olga Blud <olga.blud@gmail.com>
2024-07-09 11:13:17 +0300

    add multiple show and remove comments

--- src/js/faq.js
+++ src/js/faq.js
@@ -1,7 +1,7 @@
 import Accordion from 'accordion-js';
-// import 'accordion-js/dist/accordion.min.css';
-// new Accordion('.accordion-container');
 
 import { refs } from './refs';
 
-new Accordion(refs.faqList);
+new Accordion(refs.faqList, {
+  showMultiple: true,
+});
