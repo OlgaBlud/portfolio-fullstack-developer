6589692bcde090fc4ff54eeb6b85237df4080e74
Olga Blud <olga.blud@gmail.com>
2024-07-09 11:51:28 +0300

    add specific css for swiper

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
-
 import './js/about-me-accordion';
 import './js/about-me-swiper';
 import './js/work-together';
 import './js/reviews';
 import './js/header';
 import './js/covers';
+import './js/faq';
