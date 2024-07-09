33ce88ed2361603ef29d2cf098e7fafa5d5bbefc
Olga Blud <olga.blud@gmail.com>
2024-07-09 11:26:22 +0300

    resolve conflict refs

--- src/js/refs.js
+++ src/js/refs.js
@@ -2,7 +2,7 @@ export const refs = {
   btnClose: document.querySelector('.close-btn'),
   btnOpenMenu: document.querySelector('.btn-open-menu'),
   modalOverlay: document.querySelector('.modal-overlay'),
-  modal: document.querySelector('.modal'),
+  modal: document.querySelector('.modal-header'),
   close1: document.querySelector('.close1'),
   close2: document.querySelector('.close2'),
   close3: document.querySelector('.close3'),
@@ -13,5 +13,7 @@ export const refs = {
   skillsList: document.querySelector('.skills-list'),
   aboutMeList: document.querySelector('.about-me-list'),
   skillsSwiper: document.querySelector('.skills-swiper'),
+  marqueeLine: document.querySelectorAll('.marquee__line'),
+  coversSection: document.querySelector('.covers-section'),
   faqList: document.querySelector('.faq-list'),
 };
