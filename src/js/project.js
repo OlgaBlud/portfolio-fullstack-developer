e36df93a2f2f325287406baeb2475a938b5a2b8a
Olga Blud <olga.blud@gmail.com>
2024-07-11 19:23:35 +0300

    in progress

--- src/js/project.js
+++ src/js/project.js
@@ -2,21 +2,29 @@ import { refs } from './refs';
 import Swiper from 'swiper/bundle';
 
 new Swiper(refs.projectsList, {
-  direction: 'horizontal',
-
-  loop: true,
   touch: true,
   slidesPerView: 1,
   slideClass: 'project-swiper-slide',
   wrapperClass: 'project-swiper-wrapper',
   slideActiveClass: 'project-swiper-slide-active',
   navigation: {
-    nextEl: '.project-slider-prev-btn',
-    prevEl: '.project-slider-next-btn',
+    nextEl: '.project-slider-next-btn',
+    prevEl: '.project-slider-prev-btn',
   },
   keyboard: {
     enabled: true,
     onlyInViewport: false,
   },
-
+  // addition
+  spaceBetween: 16,
+  breakpoints: {
+    768: { spaceBetween: 64 },
+  },
+  keyboard: {
+    enabled: true,
+    onlyInViewport: true,
+  },
+  mousewheel: {
+    invert: true,
+  },
 });
